9d14f134c82a5cbfe926f9fb2412e8e6bf30307e
sumi17code <sumiakter17@gmail.com>
2025-09-16 20:42:52 +0600

    summery added

---
+++ src/summery.jsx
@@ -0,0 +1,11 @@
+
+
+///* 
+// component ---> a small unit a website: just JS function
+// jsx ---> html like js
+// conditional rendering ---> 6 types
+// how to loop through and array and show component
+// 
+// */
+
+  
